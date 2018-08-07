5482739eb3bfaf013c671cd0ce5fd427f0787ff7
Benjamin M6 <benjamynfrank101@gmail.com>
2018-08-07 10:40:20 +0100

    Load all images from JSON query into web-page

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -15,15 +15,31 @@ class UI {
         for (let i=0; i<data.profiles.items.length;i++){
             // create div for the user profile
             let userProfileDiv = document.createElement('div');
-            // TODO: create image tag to hold user image, append username span next to the image
+            //create image tag to hold user image, append username span next to the image
+            let userImage = document.createElement('img');
+            userImage.src = `${data["profiles"]["items"][i]["avatar_url"]}`;
+            userImage.alt = `${data["profiles"]["items"][i]["login"]}`;
+            userImage.style.width = '52px';
+            userImage.style.height = '52px';
 
-            // span for the user name
+            // span for the user name text
             let username = document.createElement("span");
+
             // append user name from the json data query
             username.append(`${data["profiles"]["items"][i]["login"]}`);
-            // append the username text from json data
+            username.className = 'mdl-color-text--blue-grey-50 ml-2';
+
+            // append the username text and image from json data
+            userProfileDiv.append(userImage);
             userProfileDiv.append(username);
-            // append element to parent node
+            userProfileDiv.className = "mt-4";
+
+            // TODO: Create Link to launch github user profile
+            // TODO: Arrange the ui
+            // Todo: use rounded corner images
+            // Todo: fix padding and margin
+
+            // append element to parent node'
             document.getElementById('profiles').appendChild(userProfileDiv);
         }
     }
